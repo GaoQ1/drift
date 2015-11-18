dc7100de4ea7705f41719b2a4ff1413e4f22be29
GaoQ <18912936976@163.com>
2015-11-18 16:16:37 +0800

    update1118

--- app.js
+++ app.js
@@ -22,6 +22,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req,res,next){
+  res.locals.user = {
+    throwTimes:0,
+    pickTimes:0
+  };
+  next();
+})
+
 app.use('/', routes);
 app.use('/users', users);
 
